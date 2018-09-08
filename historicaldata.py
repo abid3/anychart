05b880f1b8e20e3a6ac72e1b0048e55978bdc401
Abid Hussain <mdabidhussainbth@gmail.com>
2018-09-08 16:24:26 +0530

    Add: INDEX DATA

--- historicaldata.py
+++ historicaldata.py
@@ -7,16 +7,17 @@ API_ENDPOINT = "http://115.112.230.27:8004/api/TCService/GetDateWiseSymbolData"
 # data to be sent to api
 data = {
     "LoginId": "SUJIT",
-    "GatewayId": "NSECM",
-    "Exchange": "NSECM",
-    "Symbol": "NIFTY50",
-    "StartTime": "27/08/2018 9:15am",
-    "EndTime": "29/08/2018 3:15pm",
-    "minute": "15"
+    "GatewayId": "NSEFO",
+    "Exchange": "NSEFO",
+    "Symbol": "NIFTY",
+    "StartTime": "01/08/2018 9:15am",
+    "EndTime": "07/09/2018 3:15pm",
+    "minute": "60"
 }
 
 # sending post request and saving response as response object
 r = requests.post(url=API_ENDPOINT, data=data)
 # extracting response text
-pastebin_url = r.json()
+pastebin_url = r.content
+
 print("The pastebin URL is:%s" % pastebin_url)
